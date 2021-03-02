98e8cfc3462aba39843fe6a4550951c917b60165
DP94 <d.pell1994@gmail.com>
2021-03-02 23:16:19 +0000

    Resolve Angular compilation issues

--- DonateCraftNG/src/environments/environment.prod.ts
+++ DonateCraftNG/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: '/',
+  fullAPIUrl: 'http://localhost:8000/',
   // Until we're completely happy with the flow and has been tested enough
   // Prod file will still reference staging. Can change once we're actively playing
   justGivingAPIUrl: 'https://api.staging.justgiving.com/redacted/v1',
--- DonateCraftNG/src/environments/environment.ts
+++ DonateCraftNG/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/',
+  fullAPIUrl: 'http://localhost:8000/',
   justGivingAPIUrl: 'https://api.staging.justgiving.com/redacted/v1',
   justGivingDonateUrl: 'https://link.staging.justgiving.com/v1/charity/donate/charityId/'
 };

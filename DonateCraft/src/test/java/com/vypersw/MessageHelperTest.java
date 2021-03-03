76fea1c1169dc087b793b51901a1366f60e82bda
veronica <veronica.vandevyver@gmail.com>
2021-03-03 22:44:49 +0000

    Added new test.

--- DonateCraft/src/test/java/com/vypersw/MessageHelperTest.java
+++ DonateCraft/src/test/java/com/vypersw/MessageHelperTest.java
@@ -76,6 +76,15 @@ public class MessageHelperTest {
     }
 
     @Test
+    public void testThatDonationFormatsCorrectlyIfWholeNumber(){
+        donation.setAmount(10.00);
+        String result = messageHelper.getDonationMessageFromRevival(player, revival);
+        //§ is inserted by ChatColor.COLOR, i.e. ChatColor.WHITE is §f
+        String expected = "§6Test§f just donated§a £10.00§f to §6Test charity§f! They will be revived shortly (if they are online)";
+        assertEquals(expected, result);
+    }
+
+    @Test
     public void testSendDeathURL() {
       messageHelper.sendDeathURL(player);
 

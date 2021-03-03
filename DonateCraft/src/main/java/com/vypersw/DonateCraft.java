638fc33387437c0d5fb86a1148b24a8fdd191780
Sam <samkio@tutanota.com>
2021-03-03 15:58:38 +0000

    Make server url configurable
    This was done such that the backend and frontend
    URLs can be changed via config. Closes #21

--- DonateCraft/src/main/java/com/vypersw/DonateCraft.java
+++ DonateCraft/src/main/java/com/vypersw/DonateCraft.java
@@ -5,18 +5,19 @@ import com.vypersw.listeners.PlayerListener;
 import com.vypersw.network.HttpHelper;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ResourceBundle;
-
 public class DonateCraft extends JavaPlugin {
 
     public static final int TICKS_TO_SECONDS = 20;
 
     @Override
     public void onEnable() {
-        ResourceBundle bundle = ResourceBundle.getBundle("env");
-        final String serverUrl = bundle.getString("SERVER_URL");
-        HttpHelper httpHelper = new HttpHelper(serverUrl);
-        MessageHelper messageHelper = new MessageHelper(serverUrl);
+        this.saveDefaultConfig();
+
+        final String backendServerURL = this.getConfig().getString("backendUrl");
+        final String frontendServerURL = this.getConfig().getString("frontendUrl");
+
+        HttpHelper httpHelper = new HttpHelper(backendServerURL);
+        MessageHelper messageHelper = new MessageHelper(frontendServerURL);
         getServer().getPluginManager().registerEvents(new PlayerListener(messageHelper, httpHelper), this);
         this.getCommand("dc").setExecutor(new DonateCraftCommands(messageHelper));
         ReanimationProtocol reanimationProtocol = new ReanimationProtocol(getServer(), messageHelper, httpHelper);

2fb1d7641a7662a6be58161019406a0d99ab9076
veronica <veronica.vandevyver@gmail.com>
2021-03-03 20:35:14 +0000

    Added missing import.

--- DonateCraft/src/main/java/com/vypersw/MessageHelper.java
+++ DonateCraft/src/main/java/com/vypersw/MessageHelper.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import java.text.DecimalFormat;
 
 public class MessageHelper {
 
